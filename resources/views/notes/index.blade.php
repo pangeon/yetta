9502588f82d8aef8ce8cf3713ab55c7266c418bf
Kamil Cecherz <kamil.cecherz@gmail.com>
2022-09-07 17:55:49 +0200

    first look

---
+++ resources/views/notes/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+    <section class="masthead page-section portfolio" id="portfolio">
+        <div class="container">
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Lista wpisów</h2>
+            <div class="divider-custom">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+        </div>
+    </section>
+@endsection
